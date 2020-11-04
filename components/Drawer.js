7b681069acdbea75c103bf0752516232b659613a
Lucas Kim <lk5jk@virginia.edu>
2020-11-04 00:17:44 -0500

    All Overviews

--- components/Drawer.js
+++ components/Drawer.js
@@ -3,10 +3,14 @@ import {createAppContainer} from 'react-navigation';
 
 import Home from '../pages/Home';
 import BudgetOverview from '../pages/BudgetOverview';
+import ExpenseOverview from '../pages/ExpenseOverview';
+import IncomeOverview from '../pages/ExpenseOverview';
 
 const Drawer = createDrawerNavigator({
     Home: {screen: Home},
-    Budget: {screen: BudgetOverview}
+    Budget: {screen: BudgetOverview},
+    Expense: {screen: ExpenseOverview},
+    Income : {screen: IncomeOverview}
 });
 
 export default createAppContainer(Drawer);
